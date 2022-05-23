aad1bd81e17d2096b59a003b2951c888befb7f16
DaiNiusCer <d.cerkauskas@gmail.com>
2022-05-23 12:28:27 +0300

    pasiruošimas atsiskaitymui

---
+++ app.js
@@ -0,0 +1,33 @@
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
+import express from 'express'
+import path from 'path'
+//Moduliai
+
+//Konstantos
+const PORT = process.env.PORT1 || 8080;
+const app = express();
+//Konstantos
+
+//View engine EJS nustatymai
+app.set('views', path.join('views'));
+app.set('view engine', 'ejs')
+//View engine EJS nustatymai
+
+//Papildomi nustatymai
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}))
+app.use(cookieParser())
+//Papildomi nustatymai
+
+//Papildomų aplankų nurodymas
+app.use(express.static('public'))
+
+//Router nurodymas
+
+//Router nurodymas
+
+//Serverio paleidimas
+app.listen(PORT, console.log(`*** Server is ON ${PORT} PORT ***`))
